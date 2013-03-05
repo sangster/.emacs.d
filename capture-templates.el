25269563fa2ab5cfefc7c45ee0f024e2ed52c9b1
sangster <jon@ertt.ca>
2013-03-05 19:44:54 -0400

    add completions for existing jiras, and show jira summary in status bar

--- capture-templates.el
+++ capture-templates.el
@@ -1,3 +1,8 @@
+(defun sangster/get-all-property-values (prop)
+  "In the current buffer, return all the values for the given prop"
+  (org-map-entries (lambda () (org-entry-get (point) prop)) (concat "+" prop "={.+}") 'file))
+
+
 (defun sangster/find-jira-point ()
 
   (goto-char (point-min))
@@ -10,27 +15,34 @@
   
   (forward-char)
   
-  (let* ((jira-tag (completing-read "Jira: " '(("PVT-" 1) ("EDR-" 2)) nil nil))
+  (let* ((completions (append (sangster/get-all-property-values "ID") '("PVT-" "EDR-")))
+         (jira-tag (completing-read "Jira: " completions nil nil))
          (jira-marker (org-find-entry-with-id jira-tag)))
-    (if jira-marker
+    (if jira-marker ; if jira exists in the file
+        ; then just go to it and add a new entry
         (progn (goto-char jira-marker)
                (forward-char)
                (org-end-of-subtree t))
 
+      ; else, add the new heading for it and download its summary
       (org-insert-subheading t)
       (insert jira-tag)
 
       (lexical-let ((summary-marker (point-marker)))
         (org-entry-put (point) "ID" jira-tag)
-      
-        (jira/fetch-summary jira-tag
+
+        (jira/fetch-summary jira-tag ; grab summary for this jira
                             (lambda (summary)
                               (with-current-buffer (marker-buffer summary-marker)
                                 (save-excursion
                                   (goto-char (marker-position summary-marker))
                                   (insert ": " summary))))))
-      
-      (org-end-of-subtree t))))
+
+      (org-end-of-subtree t))
+
+    (save-excursion ; show jira summary we are capturing for
+      (outline-up-heading 1 t)
+      (message "%s" (buffer-substring (line-beginning-position) (line-end-position))))))
 
 ; --------------------
 
